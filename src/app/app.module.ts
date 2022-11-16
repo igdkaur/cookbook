8ae1849dfefd0bc0b14f09b4c7b151f06a09324b
GDK <igdkaur@gmail.com>
2022-11-16 11:50:11 +0530

    1.5 register component and display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent 
   ],
   imports: [
     BrowserModule
@@ -14,3 +16,5 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+//register features here in declarations array
