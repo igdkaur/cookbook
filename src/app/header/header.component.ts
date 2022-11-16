cf2dbaf2ac445c5886bbb73f487927af295ee2da
GDK <igdkaur@gmail.com>
2022-11-16 11:43:11 +0530

    1.2 add decorator to component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,11 @@
+import { Component } from "@angular/core";
 
+@Component()
 export class HeaderComponent {
 
 }
 
 
 // component is a ts class
+// needs decorator to be recognised a component
+// decorator needs import from ang/core
