3445e06a308e3f96baa0c6b3d1c621a55a8b3b69
GDK <igdkaur@gmail.com>
2022-11-17 21:40:59 +0530

    2.4 recipe model

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,14 @@
+// define how single recipe looks like
+//model is a bp for objects
+
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, desc : string, imagePath: string) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imagePath;
+  }
+}
