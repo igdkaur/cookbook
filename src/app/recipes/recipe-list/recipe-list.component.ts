58e1fe06346bda4dd0a0f9a5cd42355d3918e532
GDK <igdkaur@gmail.com>
2022-11-17 22:21:38 +0530

    5 output list of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Recipe One', 'this is description','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574')
+    ,new Recipe('Recipe One', 'this is description','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574')
   ]; // here type of recipe is the model we defined // to be specific recipe would hold array of Recipe objects
  
  
