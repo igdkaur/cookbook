268c5d1a194523aeb457f704a7c2f391db49f076
GDK <igdkaur@gmail.com>
2022-11-17 22:14:32 +0530

    4 added content for recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = []; // here type of recipe is the model we defined // to be specific recipe would hold array of Recipe objects
+  recipes: Recipe[] = [
+    new Recipe('Recipe One', 'this is description','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574')
+  ]; // here type of recipe is the model we defined // to be specific recipe would hold array of Recipe objects
+ 
+ 
   constructor() { }
 
   ngOnInit(): void {
