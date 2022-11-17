32945c5231e72fff899ad3c7cb5dca452920c72a
GDK <igdkaur@gmail.com>
2022-11-17 22:00:27 +0530

    3 define type for recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = []; // here type of recipe is the model we defined // to be specific recipe would hold array of Recipe objects
   constructor() { }
 
   ngOnInit(): void {
