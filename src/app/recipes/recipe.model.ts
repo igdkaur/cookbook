047aaa68934e33ebd09dcb94b94eb227a608cbe4
GDK <igdkaur@gmail.com>
2022-12-12 12:49:46 +0530

    display list of ingredients in recipe detail

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,14 +1,18 @@
 // define how single recipe looks like
 //model is a bp for objects
 
+import { Ingredient } from "../shared/ingredient.model";
+
 export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, desc : string, imagePath: string) {
+  constructor(name: string, desc : string, imagePath: string, ingredients: Ingredient[]) {
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,26 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
 
   recipeSelected= new EventEmitter<Recipe>();
-  
+
   private recipes: Recipe[] = [
-    new Recipe('Recipe One', 'This is description ','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574')
-    ,new Recipe('Recipe Two', 'this is description ','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574')
+    new Recipe(
+      'Recipe One', 
+      'This is description ',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Fries',20)
+      ])
+    ,
+    new Recipe('Recipe Two', 'this is description ','https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574',
+    [
+      new Ingredient('Meat', 1),
+      new Ingredient('Fries',20)
+    ])
   ]; 
  
   getRecipes() {
