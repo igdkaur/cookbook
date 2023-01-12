d67db3cba004aaff54df7f0eeee671dcee391f13
GDK <igdkaur@gmail.com>
2023-01-12 09:54:41 +0530

    wip needs fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes',pathMatch: 'full'},
   {path: 'recipes', component: RecipesComponent, children: [
     { path:'', component: RecipeStartComponent},
-    {path:'id', component: RecipeDetailComponent}
+    {path:':id', component: RecipeDetailComponent}
   ]},
   {path: 'shopping-list', component: ShoppingListComponent}
 ]; 
