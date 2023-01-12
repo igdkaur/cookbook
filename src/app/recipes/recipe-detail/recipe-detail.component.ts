83e7e7fd1654a86e8822b1d601538d7fa72423d7
GDK <igdkaur@gmail.com>
2023-01-12 15:46:29 +0530

    edit page navigation

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -12,7 +12,9 @@ export class RecipeDetailComponent implements OnInit {
   // @Input() recipe:Recipe;
   recipe:Recipe;
   id:number;
-  constructor(private recipeService:RecipeService, private route: ActivatedRoute) { }
+  constructor(private recipeService:RecipeService, 
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
     // const id = this.route.snapshot.params['id'];
@@ -24,11 +26,14 @@ export class RecipeDetailComponent implements OnInit {
     )
   }
   
-  
-
-  onAddToSl() {
+    onAddToSl() {
     this.recipeService.addIngredientsToSL(this.recipe.ingredients)
   }
+
+  onEditRecipe() {
+    // this.router.navigate(['edit'],{relativeTo: this.route});
+    this.router.navigate(['../',this.id,'edit'], {relativeTo: this.route})
+    }
 }
 
 // get single recipe,
