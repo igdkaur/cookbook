af0854db81288629d82606747843b30732aa65f9
GDK <igdkaur@gmail.com>
2023-01-12 15:27:47 +0530

    get route paramters for edit mode

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode =  false;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.editMode = +params['id'] != null;
+        // console.log("edit",this.editMode)
+      }
+    )
   }
 
 }
+
+// ID will only be not undefined if we are in edit mode,
+// because then an ID will be present.
