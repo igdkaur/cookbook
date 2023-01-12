b46daa4a5a4b3cd6701f039ebc99b14cea256cbf
GDK <igdkaur@gmail.com>
2023-01-12 15:35:55 +0530

    new recipe button fntnly

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit,EventEmitter, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -11,12 +12,17 @@ export class RecipeListComponent implements OnInit {
   // @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe[];
 
-  constructor(private recipeService:RecipeService) { }
+  constructor(private recipeService:RecipeService, 
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
   }
   
+  onNewRecipe() {
+    this.router.navigate(['new'],{relativeTo: this.route}); 
+  }
   // onRecipeSelected(recipe:Recipe) {
   //   this.recipeWasSelected.emit(recipe);
   // }
